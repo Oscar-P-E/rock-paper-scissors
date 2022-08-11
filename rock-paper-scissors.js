431979207e902f71d0f832fa3f40009b10f5be6c
Oscar Elmahdy <opelm1@gmail.com>
2022-08-11 16:50:58 +0800

    Add plan/outline

---
+++ rock-paper-scissors.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+  // randomly return RPS
+  // console.log check output is correct
+}
+
+// function playRound() plays a round of RPS
+
+// prompt() user input selection (case INsensitive)
+
+// takes computer (random) selection
+
+// return string that says you win/lose/draw
+
+// function game()
+// game() calls playRound() and plays 5 rounds, keeping score, and reports overall winner.
+// use console.log to display winner at end of each round
+
+// feel free to create any additional helper functions if wanted.
